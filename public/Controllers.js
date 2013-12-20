a4cb06955e869cf3a3b95c5494b359d7267e821e
root <root@78d4e647e156.(none)>
2013-12-20 01:05:46 +0000

    Use angularjs to display any user's github profile

--- public/Controllers.js
+++ public/Controllers.js
@@ -1,5 +1,21 @@
 var myAppControllers = angular.module('myAppControllers', []);
-myAppControllers.controller('user_profile',function($scope){
+myAppControllers.controller('user_profile',function($scope,$http){
 $scope.test ="hey";
+$scope.githubId="";
+$scope.getProfile = function()
+{$scope.data="";
+$http({method: 'GET', url: 'https://api.github.com/users/'+$scope.githubId}).
+  success(function(data, status, headers, config) {
+console.log(data);
+$scope.data=data;
+$scope.data= angular.fromJson(data);
+
+  }).
+  error(function(data, status, headers, config) {
+$scope.data="You entered a wrong id!"
+  });
+
+};
+
 
 });
